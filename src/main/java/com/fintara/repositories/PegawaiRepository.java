4c00eab479f4ca9ad860bc3dc44a0a288d064434
Chandra Nugrahanindhito <chandranindhito@students.undip.ac.id>
2025-06-01 00:09:52 +0700

    CI/CD github action : 1 Branch 1 BM

--- src/main/java/com/fintara/repositories/PegawaiRepository.java
+++ src/main/java/com/fintara/repositories/PegawaiRepository.java
@@ -2,12 +2,20 @@ package com.fintara.repositories;
 
 import com.fintara.models.Branch;
 import com.fintara.models.PegawaiDetails;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PegawaiRepository extends JpaRepository<PegawaiDetails, Long> {
     boolean existsByNip(String nip);
 
-    boolean existsByBranchAndRoleName(Branch branch, String roleName);
+    @Query("""
+    SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END
+    FROM PegawaiDetails p
+    WHERE p.branch = :branch AND p.user.role.name = :roleName
+    """)
+    boolean existsByBranchAndRoleName(@Param("branch") Branch branch, @Param("roleName") String roleName);
+
 }
